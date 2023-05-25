d7dbf7c981d93627cc183ab7c7d7d6875ff32084
imTypicalT <typicalt.info@gmail.com>
2023-05-24 20:50:54 -0700

    added comment link to tutorial video

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/components/CurrentWeather';
 import UpcomingWeather from './src/components/UpcomingWeather';
 
+// Continue Tutorial https://youtu.be/obH0Po_RdWk?t=3126
+
 const App = () => {
   return (
     <View style={styles.container}>
