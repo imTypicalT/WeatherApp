6bd879eb21ff77c8aa731a903f0bb2864deaebd2
imTypicalT <typicalt.info@gmail.com>
2023-05-24 19:43:44 -0700

    added styles and vector icon

--- App.js
+++ App.js
@@ -1,22 +1,68 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from 'react'
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-export default function App() {
-  const message = 'Hello!'
+const App = () => {
   return (
-    <View style={{height: 100, width: 100, backgroundColor: 'green'}}>
-      <Text>{message}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+        {/* Temperature */}
+        <Text style={styles.temp}>73F</Text>
+        {/* Feels Like */}
+        <Text style={styles.feels}>Feels Like: 76F</Text>
+        {/* High & Low */}
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 78F </Text>
+          <Text style={styles.highLow}>Low: 59F</Text>
+        </View>
+        {/* Description */}
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>It's sunny</Text>
+          <Text style={styles.message}>It's perfect t-shirt weather</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
+// Icon & Background Color
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black',
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40,
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
   },
 });
+export default App;
