c78d59b0618b9f5bffc4f5fbcc41bfa2520fc1b1
imTypicalT <typicalt.info@gmail.com>
2023-05-26 15:53:23 -0700

    removed item function, moved to ListItem.js

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -8,7 +8,7 @@ import {
   StatusBar,
   ImageBackground,
 } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import ListItem from '../components/ListItem';
 
 const DATA = [
   {
@@ -49,22 +49,10 @@ const DATA = [
   },
 ];
 
-const Item = (props) => {
-  const { dt_txt, min, max, condition } = props;
-  return (
-    <View style={styles.item}>
-      <Feather name={'sun'} size={50} color={'white'} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
-    </View>
-  );
-};
-
 // Render Screen
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => (
-    <Item
+    <ListItem
       condition={item.weather[0].main}
       dt_txt={item.dt_txt}
       min={item.main.temp_min}
@@ -92,29 +80,12 @@ const UpcomingWeather = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+
     backgroundColor: 'royalblue',
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    borderWidth: 5,
-    backgroundColor: 'pink',
-  },
-  temp: {
-    color: 'white',
-    fontSize: 20,
-  },
-  date: {
-    color: 'white',
-    fontSize: 15,
   },
   image: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
 });
 export default UpcomingWeather;
