d6023b3245997bda0109928c3d5f26fd9ead12ac
imTypicalT <typicalt.info@gmail.com>
2023-05-29 14:23:04 -0700

    created to display different weather types

---
+++ src/utilities/weatherType.js
@@ -0,0 +1,42 @@
+export const weatherType = {
+  Thunderstorm: {
+    icon: 'zap',
+    message: 'Expect some thunderstorms',
+    backgroundColor: '#000000',
+  },
+  Drizzle: {
+    icon: 'cloud-rain',
+    message: 'Expect an off and on drizzle',
+    backgroundColor: '#36454f',
+  },
+  Rain: {
+    icon: 'umbrella',
+    message: 'Bring an umbrella',
+    backgroundColor: '#0000ff',
+  },
+  Snow: {
+    icon: 'cloud-snow',
+    message: "Let's build a snowman!",
+    backgroundColor: '#7f6065',
+  },
+  Clear: {
+    icon: 'sun',
+    message: "It's perfect t-shirt weather",
+    backgroundColor: '#e47200',
+  },
+  Clouds: {
+    icon: 'cloud',
+    message: "It's very, very cloudy",
+    backgroundColor: '#363636',
+  },
+  Haze: {
+    icon: 'wind',
+    message: 'Expect haze today',
+    backgroundColor: '#58586e',
+  },
+  Mist: {
+    icon: 'align-justify',
+    message: 'Expect low visibility',
+    backgroundColor: '#3e3e37',
+  },
+}
