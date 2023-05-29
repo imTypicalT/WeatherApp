315a9c237bbdef91f0273ff5751b776ceefe47dd
imTypicalT <typicalt.info@gmail.com>
2023-05-29 10:47:26 -0700

    render Cities.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
+import City from './src/screens/Cities';
 
 // Continue Tutorial https://youtu.be/obH0Po_RdWk?t=6173
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UpcomingWeather />
+      <City />
     </View>
   );
 };
