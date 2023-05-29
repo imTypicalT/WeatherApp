83733ac988873dd93aabad9857f6bdd2bb26105e
imTypicalT <typicalt.info@gmail.com>
2023-05-29 13:14:02 -0700

    changed auto-indent space count

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  bracketSpacing: true,
-  bracketSameLine: true,
-  singleQuote: true,
-  tabWidth: 4,
-  useTabs: false,
-  trailingComma: 'es5',
-  semi: true,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    semi: true,
 };
