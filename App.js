f7efb3fd9247704710ec4d839a62034b1970d650
imTypicalT <typicalt.info@gmail.com>
2023-05-29 13:14:57 -0700

    insalled react-native-screens & safe-area-context

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
 import City from './src/screens/City';
+import { NavigationContainer } from '@react-navigation/native';
 
 // Continue Tutorial https://youtu.be/obH0Po_RdWk?t=6173
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <City />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather />
+      </View>
+    </NavigationContainer>
   );
 };
 
